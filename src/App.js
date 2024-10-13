b32bbb83b8f9a5df9bbbd2590dd8f212400a527d
/715104 <110711974+715104@users.noreply.github.com>
2024-10-13 18:47:27 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
     return () => {
         clearInterval(intervalRef.current);
     };
-}, [speed]);
+}, []);
 
     const startSorting = () => {
         if (isSorted(array)) {
