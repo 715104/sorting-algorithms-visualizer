092a7074235c57ea29776a0456a7cfdec62aa5e2
/715104 <110711974+715104@users.noreply.github.com>
2024-10-13 19:39:48 +0800

    Update generateNewArray.js

--- src/utils/generateNewArray.js
+++ src/utils/generateNewArray.js
@@ -1,6 +1,6 @@
-const generateNewArray = (size) => {
-    const newArray = Array.from({ length: size }, () => Math.floor(Math.random() * 100) + 50);
-    return newArray;
+const generateNewArray = (size, min = 50, max = 150) => {
+  const newArray = Array.from({ length: size }, () => Math.floor(Math.random() * (max - min + 1)) + min);
+  return newArray;
 };
 
 export default generateNewArray;
